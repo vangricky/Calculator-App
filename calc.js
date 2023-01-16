ec5dad99f6fa82bbbd6060578f3442bcfe2bd43e
Ricky Vang <vangcheemeng@yahoo.com>
2023-01-16 14:09:11 -0700

    made func init

--- calc.js
+++ calc.js
@@ -79,3 +79,10 @@ function handleNumber(numberString) {
         buffer += numberString;
     }
 }
+
+function init() {
+    document.querySelector('.calc-buttons').addEventListener('click', function(event){
+        buttonClick(event.target.innerText)
+    })
+}
+
